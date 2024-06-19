5eb6d63a57e1084e3838ca9ef4b5ef883040dfac
sclement2 <sclement2@gmail.com>
2024-06-19 17:16:07 +0000

    Finish writing data to DB with Flask

--- board/posts.py
+++ board/posts.py
@@ -41,5 +41,8 @@ def create():
 
 @bp.route("/posts")
 def posts():
-    posts = []
+    db = get_db()
+    posts = db.execute(
+        "SELECT author, message, created FROM post ORDER BY created DESC"
+    ).fetchall()
     return render_template("posts/posts.html", posts=posts)
